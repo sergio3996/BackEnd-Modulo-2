934b550e808acaf31a0089401dc83db3b3d2af7d
sergio <sergio3996@gmail.com>
2021-08-18 11:08:17 -0300

    Testing modelo

---
+++ red_bicicletas/models/bicicleta.js
@@ -0,0 +1,43 @@
+var Bicicleta = function (id, color, modelo, ubicacion){
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.prototype.toString = function (){
+    return 'id: ' + this.id + ' | color: '+ this.color;
+
+}
+
+Bicicleta.allBicis = [];
+Bicicleta.add = function(aBici){
+    Bicicleta.allBicis.push(aBici);
+}
+
+Bicicleta.findById = function(aBiciId){
+    var aBici = Bicicleta.allBicis.find(x => x.id == aBiciId);
+    if (aBici)
+        return aBici;
+    else
+        throw new Error(`No existe una bicicleta con el id ${aBiciId}`)
+}
+
+Bicicleta.removeById = function (aBiciId){
+    for(var i = 0; i<Bicicleta.allBicis.length; i++){
+        if (Bicicleta.allBicis[i].id == aBiciId){
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
+/*
+var a = new Bicicleta(1, 'rojo', 'urbana', [-34.8835, -56.1665]);
+var b = new Bicicleta(2, 'azul', 'urbana', [-34.8830, -56.1664]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+*/
+
+module.exports = Bicicleta;
